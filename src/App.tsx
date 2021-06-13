953089d39782158b3a1bc070e02acbeec8af0d0e
rhmnaulia <rahmanauliax@gmail.com>
2021-06-13 22:41:12 +0700

    init file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let data = [
+    "Andi",
+    "Abdul",
+    "Ahmad",
+    "Budiono",
+    "Budi",
+    "Rizal",
+    "Raffi",
+    "Putri",
+    "William",
+    "Zaki",
+    "Rifat",
+    "Cinta",
+    "Kirana",
+    "Samsul",
+    "Burhan",
+    "Vannisa",
+    "Meidiana",
+    "Nadiem",
+    "Nadia",
+  ];
+  const [value, setValue] = useState<string>();
+  const [list, setList] = useState<any>();
+  const setInputValue = (e: any, item: any) => {
+    setValue(item);
+  };
+  let suggestions: Array<string> = [];
+  const autocomplete = (event: any) => {
+    setValue(event.target.value);
+    data.forEach((item) => {
+      if (
+        item.substring(0, event.target.value.length).toLowerCase() ===
+        event.target.value.toLowerCase()
+      ) {
+        suggestions.push(item);
+        setList(
+          suggestions.map((item, idx) => {
+            return (
+              <p
+                className='suggestion'
+                onClick={(e) => setInputValue(e, item)}
+                key={idx}
+              >
+                {item}
+              </p>
+            );
+          })
+        );
+      }
+      if (event.target.value.length === 0) {
+        setList("");
+      }
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Autocomplete Implementation</h2>
+      <input
+        value={value}
+        type='text'
+        className='autocomplete'
+        onChange={(e) => autocomplete(e)}
+        placeholder='enter your name'
+      />
+      <div className='suggestion-box'>{list}</div>
     </div>
   );
-}
+};
 
 export default App;
